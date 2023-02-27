7068c552fcff1eb82e98d694748957a8b855789c
Andre Silva <andreswebs@pm.me>
2023-02-27 12:14:55 -0300

    enable max age from env

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,3 +1,4 @@
-const eventMaxAge = 30000 as const;
+const envEventMaxAgeMs = parseInt(String(process.env.EVENT_MAX_AGE_MS), 10);
+const eventMaxAgeMs = isNaN(envEventMaxAgeMs) ? 30000 : envEventMaxAgeMs;
 
-export { eventMaxAge };
+export { eventMaxAgeMs };
